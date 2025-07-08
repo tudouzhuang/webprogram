56695bcbfb237b5eedf7a6760e423a21b186e7ce
Avocadozhuang <tudouzhuang@gmail.com>
2025-07-08 14:21:54 +0800

    'init'

---
+++ tcr/project/src/main/java/org/example/project/Controller/PageController.java
@@ -0,0 +1,34 @@
+package org.example.project.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/login")
+    public String toLogin() {
+        return "forward:/static/login.html";
+    }
+
+    @GetMapping("/signup")
+    public String toSignup() {
+        return "forward:/static/signup.html";
+    }
+
+    @GetMapping("/reset")
+    public String toReset() {
+        return "forward:/static/reset-password.html";
+    }
+
+    @GetMapping("/index")
+    public String toIndex() {
+        return "forward:/static/index.html";
+    }
+
+    @GetMapping("/404")
+    public String to404() {
+        return "forward:/static/404.html";
+    }
+
+} 
